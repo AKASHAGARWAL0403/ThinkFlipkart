b8e01c3df1dffcdd9775f72c25938716c5e56c72
Akash Agarwal <akashagarwal0403@gmail.com>
2020-04-30 16:28:02 +0530

    Bug Fixed in BugFix Branch 	Number.c 		Changed Sum and Sub function 	Hello.c 		Added toLower

--- SourceCode/Number/Number.c
+++ SourceCode/Number/Number.c
@@ -2,11 +2,17 @@
 #include<math.h>
 
 int add(int a , int b){
-	 return a+b;
+	//Added in bugFix branch
+	int result = 0;
+	result = a + b;
+	return result;
 }
 
 int sub(int a, int b){
-	return a-b;
+	//Added in bugFix branch
+	int result = 0;
+	result = a-b;
+	return result;
 }
 
 //Added in testing branch
